cc1e0387bf66f6d05092cb0ab95d55fb937bb342
DzmitreyDanilau <dzmitrey.danilau@gmail.com>
2019-11-18 23:54:19 +0300

    +Notification channel creation inside Broadcast reciever +Notificationmanager inside broadcastReciever +Extension fun for Notification manager (Notification Utils) +cancel all notification after restart timer inside EggTimerViewModel

--- app/src/main/java/com/example/android/eggtimernotifications/receiver/AlarmReceiver.kt
+++ app/src/main/java/com/example/android/eggtimernotifications/receiver/AlarmReceiver.kt
@@ -1,38 +1,48 @@
-/*
- * Copyright (C) 2019 Google Inc.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.example.android.eggtimernotifications.receiver
 
+import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.graphics.Color
+import android.os.Build
 import android.widget.Toast
 import androidx.core.content.ContextCompat
 import com.example.android.eggtimernotifications.R
 import com.example.android.eggtimernotifications.util.sendNotification
 
-class AlarmReceiver: BroadcastReceiver() {
+class AlarmReceiver : BroadcastReceiver() {
+    private lateinit var notificationManager: NotificationManager
 
     override fun onReceive(context: Context, intent: Intent) {
         // TODO: Step 1.10 [Optional] remove toast
-        Toast.makeText(context, context.getText(R.string.eggs_ready), Toast.LENGTH_SHORT).show()
+//        Toast.makeText(context, context.getText(R.string.eggs_ready), Toast.LENGTH_SHORT).show()
 
         // TODO: Step 1.9 add call to sendNotification
-
+        notificationManager = ContextCompat.getSystemService(
+            context,
+            NotificationManager::class.java
+        ) as NotificationManager
+        createChannel(
+            context.getString(R.string.egg_notification_channel_id),
+            context.getString(R.string.breakfast_notification_channel_name)
+        )
+        notificationManager.sendNotification(context.getString(R.string.eggs_ready), context)
     }
 
+    private fun createChannel(channelId: String, channelName: String) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val notificationChannel = NotificationChannel(
+                channelId,
+                channelName,
+                NotificationManager.IMPORTANCE_LOW
+            )
+            notificationChannel.enableLights(true)
+            notificationChannel.lightColor = Color.RED
+            notificationChannel.enableLights(true)
+            notificationChannel.enableVibration(true)
+            notificationManager.createNotificationChannel(notificationChannel)
+        }
+    }
 }
--- app/src/main/java/com/example/android/eggtimernotifications/util/NotificationUtils.kt
+++ app/src/main/java/com/example/android/eggtimernotifications/util/NotificationUtils.kt
@@ -24,10 +24,14 @@ private val FLAGS = 0
 fun NotificationManager.sendNotification(messageBody: String, applicationContext: Context) {
     // Create the content intent for the notification, which launches
     // this activity
-    // TODO: Step 1.11 create intent
-
+    val contentIntent = Intent(applicationContext, MainActivity::class.java)
     // TODO: Step 1.12 create PendingIntent
-
+    val contentPendingIntent = PendingIntent.getActivity(
+        applicationContext,
+        NOTIFICATION_ID,
+        contentIntent,
+        PendingIntent.FLAG_UPDATE_CURRENT
+    )
     // TODO: Step 2.0 add style
 
     // TODO: Step 2.2 add snooze action
@@ -49,6 +53,9 @@ fun NotificationManager.sendNotification(messageBody: String, applicationContext
                 .getString(R.string.notification_title)
         )
         .setContentText(messageBody)
+        .setContentIntent(contentPendingIntent)
+        .setAutoCancel(true)
+        .setOnlyAlertOnce(true)
 
     // TODO: Step 1.13 set content intent
 
@@ -64,3 +71,6 @@ fun NotificationManager.sendNotification(messageBody: String, applicationContext
 }
 
 // TODO: Step 1.14 Cancel all notifications
+fun NotificationManager.cancelAllNotifications(){
+    cancelAll()
+}
